4aed7f8c537fd8ba9f565c0e5ddca6924c8a4e69
mihirandani <122155059+mihirandani@users.noreply.github.com>
2023-01-07 17:27:33 +0530

    Add files via upload

---
+++ module2/even odd.py
@@ -0,0 +1,25 @@
+
+
+
+
+a=int(input("enter chack your number"))
+
+
+
+if (a % 2) == 0:
+    print (" even number")
+
+else:
+    print("odd number")
+
+
+
+
+m=int(input("enter your number"))
+
+if (m % 2) == 0:
+    print("even number")
+
+else :
+    print("odd number")
+2
